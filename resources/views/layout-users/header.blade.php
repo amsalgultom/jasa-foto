362ab2c19b9fc7f343d15e5f670b332c18bff93e
hayyu <hayyudwi03@gmail.com>
2023-06-20 12:53:31 +0700

    fix navbar

--- resources/views/layout-users/header.blade.php
+++ resources/views/layout-users/header.blade.php
@@ -11,57 +11,46 @@
         <li><a href="{{ route('home') }}">Product</a></li>
         <li><a href="{{ route('ourservices') }}">Our Services</a></li>
         <hr>
-        @if (Auth::user())
-        <div class="d-flex w-100 justify-content-right none-dflex-mobile">
-          <li><a href="{{ route('order.service') }}">Buat Order</a></li>
-          <li><a href="/myorderservices/{{ Auth::user()->id }}">Order Saya</a></li>   
-          <li><a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a></li>   
-          <form id="logout-form" class="d-none" action="{{ route('logout') }}" method="POST">
-          @csrf
-          </form>
-        </div>
-
-        </ul>
-        @else
-        <div class="d-flex w-100 justify-content-right none-dflex-mobile">
-          <li><a href="{{ route('login') }}">Login</a></li>
-          <li><a href="{{ route('register') }}">Register</a></li>   
-        </div>
-      </ul>
-      @endif
-      <i class="bi bi-list mobile-nav-toggle"></i>
-    </nav><!-- .navbar -->
-    
-
-    <nav id="navbar-auth" class="navbar navbar-auth">
         @if (Auth::user() && Auth::user()->role == 'client')
 
-        <ul>
+        <div class="d-flex w-100 justify-content-right none-dflex-mobile">
+
           <li><a href="{{ route('order.service') }}">Buat Order</a></li>
           <li><a href="/myorderservices/{{ Auth::user()->id }}">Order Saya</a></li>   
           <li><a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a></li>   
           <form id="logout-form" class="d-none" action="{{ route('logout') }}" method="POST">
           @csrf
           </form>
+        </div>
+
         </ul>
       
         @elseif(Auth::user() && Auth::user()->role == 'admin')
-        <ul>
+        <div class="d-flex w-100 justify-content-right none-dflex-mobile">
+
           <li><a href="{{ route('dashboard') }}">My Admin</a></li>
           <li><a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a></li>   
           <form id="logout-form" class="d-none" action="{{ route('logout') }}" method="POST">
           @csrf
           </form>
+        </div>
+
         </ul>
         @else
 
-        <ul>
+        <div class="d-flex w-100 justify-content-right none-dflex-mobile">
+
           <li><a href="{{ route('login') }}">Login</a></li>
           <li><a href="{{ route('register') }}">Register</a></li>   
+        </div>
+
         </ul>
         @endif
-        <i class="bi bi-person mobile-nav-auth-toggle"></i>
-      </nav>.navbar -->
+      
+      <i class="bi bi-list mobile-nav-toggle"></i>
+    </nav><!-- .navbar -->
+    
+
 
     <div class="logo text-center"><a href="{{ route('home') }}"><img src="{{ asset('img/logo.png') }}" alt=""></a></div>
 
