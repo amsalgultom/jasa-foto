321f4ef49e7b6c5d51ff33cdf04cbe6206f6d1fd
Amsal-G <94661283+Amsal-G@users.noreply.github.com>
2023-08-13 17:50:39 +0700

    add show password on login and add cod on shipping

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,18 +4,18 @@
 
 <!-- Outer Row -->
 <section class="mt-5 mb-5">
-<div class="container">
+    <div class="container">
 
-<div class="card card-auth o-hidden border-0 shadow-lg my-5">
-    <div class="card-body p-0">
-        <!-- Nested Row within Card Body -->
-        <div class="row">
-            <div class="col-lg-5 d-none d-lg-block bg-login-image"></div>
-                <div class="col-lg-7">
-                    <div class="p-5">
-                        <div class="text-center">
-                            <h1 class="h4 text-gray-900 mb-4">Login ArtSpace Photoshoot</h1>
-                        </div>
+        <div class="card card-auth o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block bg-login-image"></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Login ArtSpace Photoshoot</h1>
+                            </div>
                             <form action="{{ route('authenticate') }}" method="post">
                                 @csrf
                                 <div class="form-group">
@@ -30,11 +30,14 @@
                                     <span class="text-danger">{{ $errors->first('password') }}</span>
                                     @endif
                                 </div>
-                                <input type="submit" class="col-md-3 offset-md-5 btn btn-primary" value="Login">
+                                <div class="form-group">
+                                    <input type="checkbox" onclick="showPassword()"><span style="color: #a38f85;"> Show Password</span>
+                                </div>
+                                <input type="submit" class="col-md-3 offset-md-5 btn btn-primary mt-2" value="Login">
                                 <hr>
                             </form>
                             <div class="text-center">
-                                <a  href="/register">Create an Account!</a>
+                                <a href="/register">Create an Account!</a>
                             </div>
                         </div>
                     </div>
@@ -44,6 +47,19 @@
 
     </div>
 
-</div>
+    </div>
 </section>
 @endsection
+
+@push('scripts')
+<script>
+    function showPassword() {
+        var x = document.getElementById("password");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+</script>
+@endpush
