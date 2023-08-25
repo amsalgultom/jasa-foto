8ebc1d9b2ca75a1e2dab8f002a3857748809393a
Amsal-G <94661283+Amsal-G@users.noreply.github.com>
2023-08-25 22:59:20 +0700

    set prod midtrans

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -182,7 +182,7 @@ class OrderController extends Controller
         // Set your Merchant Server Key
         \Midtrans\Config::$serverKey = config('midtrans.server_key');
         // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        \Midtrans\Config::$isProduction = false;
+        \Midtrans\Config::$isProduction = true;
         // Set sanitization on (default)
         \Midtrans\Config::$isSanitized = true;
         // Set 3DS transaction for credit card to true
